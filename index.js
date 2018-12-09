0dedb69e7bc1f2fc79b1e6022d66e4eb86e9ecd5
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-09 14:49:30 -0600

    added multer because ill only be using images

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const express = require("express");
-const formidable = require("formidable");
+const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const port = 3333;
 
 const app = express();
 app.use(express.static("public"));
+app.use(express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/fileupload", (req, res) => {
-  var form = new formidable.IncomingForm();
-  form.parse(req);
-
-  form.on("fileBegin", function(name, file) {
-    file.path = __dirname + "/uploads/" + file.name;
-  });
-
-  form.on("file", function(name, file) {
-    console.log("Uploaded " + file.name);
-  });
-
   res.redirect("/");
 });
 
